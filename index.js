354513efbc38b87749076627210274a129f4fa45
yashvardhan90 <yash27sharma@gmail.com>
2016-10-01 23:40:58 -0500

    Adding website1

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ io.on("connection", function(socket) {
 	})
 });
 
-http.listen("3000", function() {
+http.listen(app.get("port"), function() {
 	console.log("Listening on port", app.get("port"));
 });
